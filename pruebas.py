b39c2a303428fc76809d8027361cac05c14eb9fc
Nicolas Terroni <nsterroni@gmail.com>
2020-06-08 20:07:59 -0300

    funciones con parametros, conversor de divisas

--- pruebas.py
+++ pruebas.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
-import turtle
+def foering_exchange(amount):
+    ars_to_usd_rate= 91
+    return amount / ars_to_usd_rate
 
-def main():
-    window = turtle.Screen()
-    dave = turtle.Turtle()
-    make_square(dave)
-    turtle.mainloop()
+def run():
+    print("CALCULADORA DE DIVISAS - by NicolasTerroni")
+    print("Convierte pesos argentinos a dolares.")
+    print("")
 
-def make_square(dave):
-    length = int(input('Tamaño de cuadrado: '))
+run()
 
-    for i in range(4):
-        make_line_and_turn(dave, length)
+amount=float(input("What amount of pesos do you want to convert to U$S?: "))
 
-def make_line_and_turn(dave, length):
-    dave.forward(length)
-    dave.left(90)
+resultado = foering_exchange(amount)
 
-if __name__ == '__main__':
-    main()
+print(f"${amount} pesos argentinos, son ${resultado} dolares.")
