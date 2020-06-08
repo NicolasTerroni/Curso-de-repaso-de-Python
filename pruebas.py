a6062f5c204860523ce6841c98c7047ef3c40121
Nicolas Terroni <nsterroni@gmail.com>
2020-06-08 17:19:46 -0300

    prueba de modulo turtle

---
+++ pruebas.py
@@ -0,0 +1,13 @@
+import turtle
+
+window = turtle.Screen()
+mbappe = turtle.Turtle()
+mbappe.forward(50)
+mbappe.left(90)
+mbappe.forward(50)
+mbappe.left(90)
+mbappe.forward(50)
+mbappe.left(90)
+mbappe.forward(50)
+
+turtle.mainloop()
