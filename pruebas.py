b6e317705a8123e34b92b1c547e7d77d70f2392d
Nicolas Terroni <nsterroni@gmail.com>
2020-06-08 18:50:24 -0300

    uso de funciones con turtle

--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,21 @@
+# -*- coding: utf-8 -*-
 import turtle
 
-window = turtle.Screen()
-mbappe = turtle.Turtle()
-mbappe.forward(50)
-mbappe.left(90)
-mbappe.forward(50)
-mbappe.left(90)
-mbappe.forward(50)
-mbappe.left(90)
-mbappe.forward(50)
+def main():
+    window = turtle.Screen()
+    dave = turtle.Turtle()
+    make_square(dave)
+    turtle.mainloop()
 
-turtle.mainloop()
+def make_square(dave):
+    length = int(input('Tamaño de cuadrado: '))
+
+    for i in range(4):
+        make_line_and_turn(dave, length)
+
+def make_line_and_turn(dave, length):
+    dave.forward(length)
+    dave.left(90)
+
+if __name__ == '__main__':
+    main()
