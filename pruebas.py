59850d537305a376d66b67ee8129f7bd921379e8
Nicolas Terroni <nsterroni@gmail.com>
2020-06-09 20:24:35 -0300

    Calculo de factorial usando funciones recursivas

--- pruebas.py
+++ pruebas.py
@@ -1,28 +1,12 @@
-def is_prime(number):
-    if number < 2:
-        return False
-    elif number == 2:
-        return True
-    elif number > 2 and number % 2 == 0:
-        return False
-    else:
-        for i in range(3, number):
-            if number % i == 0:
-                return False
-    return True
+# Hacer un programa que calcule el factorial de un numero usando funciones recursivas
 
-def run():
-    pass
+def calcula_fact(num):
+    #0 es nuestro caso base, si no lo hubieramos establecido entramos en bucle infinito
+    if num == 0:
+        return 1
+    return num*calcula_fact(num-1)
+#mi error al pensar este ejercicio fue creer que el return acumulaba los resultados, cuando en realidad solo multiplicaba en ciclo por el numero anterior hasta que al ser 0 le digamos que devuelva 1 (LOGIC)
+num = int(input("Ingrese el numero: "))
 
-    number = int(input("Input your number: "))
-
-    result = is_prime(number)
-
-    if result:
-        print(f"{number} is a prime number.")
-    else:
-        print(f"{number} isn't a prime number.")
-
-
-if __name__ == "__main__":
-    run()
+res = calcula_fact(num)
+print(res)
