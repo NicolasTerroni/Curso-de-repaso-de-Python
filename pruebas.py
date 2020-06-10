e546356e8a0779a6efdde24d76a28b3c6548785a
Nicolas Terroni <nsterroni@gmail.com>
2020-06-10 01:56:31 -0300

    Detector de palindromos, no use ciclos ni funciones beacose -keep it simple-

--- pruebas.py
+++ pruebas.py
@@ -1,34 +1,12 @@
-# Utilizando el programa del profesor para generar numeros aleatorios y llevarnos a el, debemos modificarlo para hacerlo mas complejo
-import random
-def play():
-    number_found = False
-    From = int(input("Desde que numero queres jugar? ----> "))
-    To = int(input("Hasta que numero queres jugar? ----> "))
-    random_number = random.randint(From, To)
-    while not number_found:
-        number = int(input('Intenta un número：'))
-        if number == random_number:
-            print('Felicidades. Encontraste el número')
-            number_found = True
-        elif number > random_number:
-            print('El número es más pequeño')
-        else:
-            print('El número es más grande')
+# Detectar si una palabra es un palindromo, lo voy a hacer en ingles porque la vida lo quiso asi
 
-again = True
-while again:
-    play()
-    play_again = input("Jugar de nuevo? Y/N ---> ")
-    if play_again.upper() == "Y":
-        again = True
-    elif play_again.upper() == "N":
-        again = False
-    else:
-        print("Debes ingresar Y o N. ")
-"""
-- Ahora podemos establecer desde donde a donde se generara el rango.
-- Podemos elegir si jugar de nuevo o no
+word = input("Input word -------------> ")
+if word == word[::-1]:
+    print(f"YES! '{word}' IS A PALINDROME")
+else:
+    print(f"No,'{word}' isn't a palindrome. You suck at this.")
 
-TARDE COMO MEDIA HORA. PERO APRENDI UN POCO MAS!
-
+""" try "larutanosaportootropasonatural" 
+        "severlasalreves"
+    sorry i dont know anyone in english
 """
