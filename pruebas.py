99a72908b40546cca4fbbec01ce1dd4b70634f96
Nicolas Terroni <nsterroni@gmail.com>
2020-06-11 02:09:01 -0300

    Ahorcado, usando funciones, ciclos condicionales. Me costo pero aprendi!

--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,114 @@
-# Detectar si una palabra es un palindromo, lo voy a hacer en ingles porque la vida lo quiso asi
+# Ahorcado usando listas, usamos ascii arts
+import random
 
-word = input("Input word -------------> ")
-if word == word[::-1]:
-    print(f"YES! '{word}' IS A PALINDROME")
-else:
-    print(f"No,'{word}' isn't a palindrome. You suck at this.")
+IMAGES = ['''
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        |
+    =========''']
 
-""" try "larutanosaportootropasonatural" 
-        "severlasalreves"
-    sorry i dont know anyone in english
-"""
+WORDS = ["python","django","framework","script","shell","scrapping","cookies","cache","editor","evernote","platzi","youtube"]
+
+
+def display(hidden_word,tries):
+    print(IMAGES[tries])
+    print("")
+    print(f"[{hidden_word}]")
+
+def random_word():
+    idx = random.randint(0 , len(WORDS) -1 )
+    return WORDS[idx]
+
+def run():
+    word = random_word()
+    hidden_word = ["-"]*len(word)
+    tries = 0
+    alive = True
+    
+    while alive:
+        display(hidden_word, tries)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        get_letter = str(input("Intenta una letra ---> "))
+
+        letter_index = []
+        
+        for idx in range(len(word)):
+            if get_letter == word[idx]:
+                letter_index.append(idx)
+        
+        if len(letter_index) == 0:
+            tries += 1
+            if tries == 7:
+                display(hidden_word, tries)
+                print("")
+                print(f"Perdiste, la palabra era: {word}")
+                alive = False
+        else:
+            for idx in letter_index:
+                hidden_word[idx] = get_letter
+            letter_index = []
+
+        if "-" not in hidden_word:
+            display(hidden_word, tries)
+            print("")
+            print(f"Ganaste! la palabra era {word}")
+            alive = False
+            
+
+if __name__ == "__main__":
+    print("------- A H O R C A D O -------")
+    run()
