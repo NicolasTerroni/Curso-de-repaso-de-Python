0d8ef53ded5cc9d14a8a3cbe989141457e952b66
Nicolas Terroni <nsterroni@gmail.com>
2020-06-17 04:13:37 -0300

    Archivos externos, cuantas veces aparece una palabra en un archivo

--- pruebas.py
+++ pruebas.py
@@ -1,23 +1,82 @@
-""" 
-Encontrar el primer caracter que no se repita adentro de un string.
-"abacabad" c
-"abacabaabacaba" _
-"abcdefghijklmnopqrstuvwxyziflskecznslkjfabe" d
-"bcccccccccccccyb" y 
+# Archivos externos, contar cuantas veces aparece una palabra en un archivo .css
+from io import open
+f = open("Desktop/prueba.txt","r")
+
+list = f.readlines()
+counter = 0
+find = input("Ingrese palabra a buscar: ")
+for line in list:
+    if find in line:
+        counter += 1
+
+print(f"La palabra '{find}', aparece {counter} veces en el archivo.")
+
+f.close()
+
 """
+file: 
 
-def finding_char(str_to_look):
-    for letter in str_to_look:
-        if str_to_look.count(letter) == 1:
-            return letter
-    return "_"
+body
+{
+    color: #333;
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+    margin: 0;
+    padding: 0;
+}
 
-if __name__ == "__main__":
-    str_to_look = str(input("Input string: "))
-    
-    result = finding_char(str_to_look)
-    
-    if result == "_":
-        print("Todos los caracteres se repiten")
-    else:
-        print(f"El primer caracter no repetido fue {result}")
+#cabecera
+{
+    text-align: center;
+    background: #6c7181;
+    box-shadow: 0px 2px 20px 0px rgba(0,0,0,0.5);
+    color: white;
+    font-weight: bold;
+    margin: 0;
+    padding: 0.5em;
+}
+
+#cabecera #logo
+{
+    text-align: center;
+    width: 20px;
+    vertical-align: middle;
+}
+
+#cabecera #tagline
+{
+    text-align: center;
+    padding: 0 0 0 1em;
+    font-weight: normal;
+    font-size: 0.8em;
+}
+
+
+#container
+{
+    width: 70%;
+    padding: 0;
+    text-align: left;
+    border: 1px solid #DDD;
+    margin: 0 auto;
+}
+
+#container h1
+{
+    font-size: 20px;
+}
+#post
+{
+    text-align: center;
+    padding: 1em;
+}
+
+#footer
+{
+    background: #777;
+    color: white;
+    text-align: center;
+    padding: 0.5em;
+}
+"""
