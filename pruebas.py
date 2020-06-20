5b56748286a387d4157ab36f52768b16b460133e
Nicolas Terroni <nsterroni@gmail.com>
2020-06-20 16:29:59 -0300

    Decoradoras

--- pruebas.py
+++ pruebas.py
@@ -1,90 +1,23 @@
-# POO (it doesn't simulate perfecly a tv but it was usefull for practicing heredity and remember how it works on python)
+# Decoradores
 
-class Electronics:
-    def __init__(self):
-         self.power = False
-    
-    def turn_on(self):
-            self.power=True
-            
-    def turn_off(self):
-            self.power=False
-            
-#---------------------------------------------------------------------------------------
-class TVs(Electronics):
-    _STATUS = [
-    """             O         O
-                        \\     // 
-                         \\   //
-                          \\ // 
-                      /~~~~~\
-            ,----------------------------,
-            | ,------------------------ , |
-            | |                              | |
-            | |                              | |
-            | |                              | |
-            | |                              | |
-            | |_______________| |
-            |_________________|
-            |_________________| """,
-    """              O         O
-                        \\     // 
-                         \\   //
-                          \\ // 
-                      /~~~~~\
-            ,----------------------------,
-            | ,------------------------ , |
-            | |                              | |
-            | |   TA PRENDIDO   | |
-            | |        JAJAJA          | |
-            | |                              | |
-            | |_______________| |
-            |_________________|
-            |_________________|"""
-]
+def decorate(func):
+    def wrapper():
+        print("-----------------------------")
+        print("Se va a realizar un calculo:")
 
-    def __init__(self, size, brand):
-        super().__init__()
-        self.screen_size = size
-        self.brand_name = brand
-        self.power=False
+        func()
 
-    def display_screen(self):
-        if self.power==False:
-            print( self._STATUS[0])
-        else: 
-            print( self._STATUS[1])
-        
-    def show_info(self):
-        print(f"Model: {self.screen_size}. Screen size: {self.screen_size}.")
+        print("Calculo realizado.")
+        print("-----------------------------")
+    return wrapper
 
-# --------------------------------------------------------------------------------------------------------------
-plasmaTV = TVs("40x20", "Samsung")
+@decorate
+def sum():
+    print(2+2)
 
-while True:
-    command = str(input("""
-    You've got the remote.. tv is off, make a move:
-    1) Turn on
-    2) Drop the remote
-    ---------> """))
-    if command == "1":
-        plasmaTV.turn_on()
-        plasmaTV.display_screen()
-        while True:
-            command = str(input("""
-            You've got the remote.. make a move:
-            1) Turn off
-            2) Show info
-            3) Drop the remote
-            ---------> """))
-            if command=="1":
-                plasmaTV.turn_off()
-                plasmaTV.display_screen()
-            elif command=="2":
-                plasmaTV.show_info()
-            else:
-                break
-    else:
-        break
-  
+@decorate
+def sub():
+    print(5-2)
 
+sum()
+sub()
